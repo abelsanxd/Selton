710480d730451071806a835faeeffe784c1065a2
Cristian Diaz <diazcristian0210@gmail.com>
2024-04-11 11:23:28 -0300

    cree una funcion con un condicional true o false si no se cumple la condicion dada

---
+++ js/helpers/validacion.js
@@ -0,0 +1,9 @@
+export function caracteresTotales (texto, min, max){
+    if (texto.length >= min && texto.length <= max){
+      
+        return true;
+    }else{
+      return false;
+    }
+  }
+  
