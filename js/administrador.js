2557df904e7040a0036cf6b5b2339e4a0172a9a5
Alud24k <ifem.perez@gmail.com>
2024-04-12 16:20:12 -0300

    Se corrio de todo un poco, links, archivos innecesarios y otras cosas

--- js/administrador.js
+++ js/administrador.js
@@ -1,6 +1,5 @@
 import Juegos from "./classJuegos.js";
 
- 
 const botonAgregarJuego = document.querySelector("#botonAgregarJuego");
 const modalJuego = new bootstrap.Modal(document.getElementById("modalAgregar"));
 let crearJuego = true;
@@ -66,41 +65,40 @@ function dibujarFila(juego) {
   console.log(juego);
   const tbody = document.querySelector("#tablajuegos");
   console.log(tbody);
-  tbody.innerHTML += `<tr>
-  <th  class="col-1" scope="row">${juego.identificador}</th>
-  <td class="col-2 text-center" >${juego.titulo}</td>
-  <td class="col-descripcion ">
-   ${juego.descripcion} 
-  </td>
-  <td class="text-center">
-   <img
-      class="img-thumbnail rounded img-fluid thumbnail "
-      src=${juego.imagen}
-      alt=${juego.titulo}
-    />  
-  </td>
-  <td class="text-center">
-    <button
-      class="btn btn-warning m-1"
-      onclick="prepararEditarJuego('${juego.identificador}')"
-  
-    >
-      <i class="bi bi-pencil-square fs-4"></i>
-    </button>
-    <button
-      class="btn btn-danger m-1"
-      onclick="borrarjuego('${juego.identificador}')"
-    >
-      <i class="bi bi-x-square fs-4"></i>
-    </button>
-  </td>
-</tr>`;
+  tbody.innerHTML += /* HTML */ `<tr>
+    <th class="col-1" scope="row">${juego.identificador}</th>
+    <td class="col-2 text-center">${juego.titulo}</td>
+    <td class="col-descripcion ">${juego.descripcion}</td>
+    <td class="text-center">
+      <img
+        class="img-thumbnail rounded img-fluid thumbnail "
+        src=${juego.imagen}
+        alt=${juego.titulo}
+      />
+    </td>
+    <td class="text-center">
+      <button
+        class="btn btn-warning m-1"
+        onclick="prepararEditarJuego('${juego.identificador}')"
+      >
+        <i class="bi bi-pencil-square fs-4"></i>
+      </button>
+      <button
+        class="btn btn-danger m-1"
+        onclick="borrarjuego('${juego.identificador}')"
+      >
+        <i class="bi bi-x-square fs-4"></i>
+      </button>
+    </td>
+  </tr>`;
 }
 
-window.prepararEditarJuego = function(identificador) {
+window.prepararEditarJuego = function (identificador) {
   mostrarModalJuego();
   crearJuego = false;
-  const juegoBuscado = juegos.find((juego) => juego.identificador === identificador);
+  const juegoBuscado = juegos.find(
+    (juego) => juego.identificador === identificador
+  );
   document.querySelector("#identificador").value = juegoBuscado.identificador;
   document.querySelector("#titulo").value = juegoBuscado.titulo;
   document.querySelector("#descripcion").value = juegoBuscado.descripcion;
@@ -110,10 +108,13 @@ window.prepararEditarJuego = function(identificador) {
 function editarJuego() {
   const identificador = document.querySelector("#identificador").value;
 
-  const posicionJuego = juegos.findIndex((juego) => juego.identificador === identificador);
+  const posicionJuego = juegos.findIndex(
+    (juego) => juego.identificador === identificador
+  );
 
   juegos[posicionJuego].titulo = document.querySelector("#titulo").value;
-  juegos[posicionJuego].descripcion = document.querySelector("#descripcion").value;
+  juegos[posicionJuego].descripcion =
+    document.querySelector("#descripcion").value;
   juegos[posicionJuego].imagen = document.querySelector("#imagen").value;
 
   guardarLocalStorage();
@@ -132,7 +133,6 @@ function editarJuego() {
   limpiarFormulario();
   modalJuego.hide();
 }
-
 
 botonAgregarJuego.addEventListener("click", mostrarModalJuego);
 formularioJuego.addEventListener("submit", administrarFormularioJuego);
