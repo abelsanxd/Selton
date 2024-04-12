a92a5ba673e706c7d8cf9f8106a4c284f29605ff
Cristian Diaz <diazcristian0210@gmail.com>
2024-04-11 22:31:05 -0300

    Agregue el add event listener para conectar el js con el html

--- js/helpers/formularioContacto.js
+++ js/helpers/formularioContacto.js
@@ -3,3 +3,9 @@ const usuario = document.getElementById('username');
 const email = document.getElementById('email');
 const telefono = document.getElementById('telefono');
 const consulta = document.getElementById ('consulta')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    checkInputs();
+});
