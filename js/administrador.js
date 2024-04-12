c4d0d19f567ff176254defaed12f5f840264b52f
Fausto Fernandez <fausto44190084@gmail.com>
2024-04-12 02:47:21 -0300

    Hice la lógica del read de la pagina administrador

--- js/administrador.js
+++ js/administrador.js
@@ -37,6 +37,7 @@ function agregandoJuego() {
   juegos.push(juegoNuevo);
   guardarLocalStorage();
   limpiarFormulario();
+  dibujarFila(juegoNuevo);
   Swal.fire({
     title: `Juego agregado!`,
 
@@ -54,6 +55,47 @@ function limpiarFormulario() {
   formularioJuego.reset();
 }
 
+function cargaInicial() {
+  if (juegos.length > 0) {
+    juegos.map((juego) => dibujarFila(juego));
+  }
+}
+
+function dibujarFila(juego) {
+  console.log(juego);
+  const tbody = document.querySelector("#tablajuegos");
+  console.log(tbody);
+  tbody.innerHTML += `<tr>
+  <th  class="col-1" scope="row">${juego.identificador}</th>
+  <td class="col-2 text-center" >${juego.titulo}</td>
+  <td class="col-descripcion ">
+   ${juego.descripcion} 
+  </td>
+  <td class="text-center">
+   <img
+      class="img-thumbnail rounded img-fluid thumbnail "
+      src=${juego.imagen}
+      alt=${juego.titulo}
+    />  
+  </td>
+  <td class="text-center">
+    <button
+      class="btn btn-warning m-1"
+      onclick="prepararEditarjuego('${juego.identificador}')"
+  
+    >
+      <i class="bi bi-pencil-square fs-4"></i>
+    </button>
+    <button
+      class="btn btn-danger m-1"
+      onclick="borrarjuego('${juego.identificador}')"
+    >
+      <i class="bi bi-x-square fs-4"></i>
+    </button>
+  </td>
+</tr>`;
+}
 
 botonAgregarJuego.addEventListener("click", mostrarModalJuego);
 formularioJuego.addEventListener("submit", administrarFormularioJuego);
+cargaInicial();
