71a122c4261f405ee7480ee03477b5283164fa07
Cristian Diaz <diazcristian0210@gmail.com>
2024-04-11 22:33:55 -0300

    agregue condicionales a las funciones de usuario, email, telefono y consulta

--- js/helpers/formularioContacto.js
+++ js/helpers/formularioContacto.js
@@ -11,10 +11,47 @@ form.addEventListener('submit', e => {
 });
 
 function checkInputs() {
-    // trim para remover los espacios del inicio
+    // trim to remove the whitespaces
     const usuarioValue = usuario.value.trim();
     const emailValue = email.value.trim();
     const telefonoValue = telefono.value.trim();
     const consultaValue = consulta.value.trim();
+    
+    if(usuarioValue === '') {
+        setErrorFor(usuario, 'No puede dejar el usuario en blanco');
+    } else {
+        setSuccessFor(usuario);
+    }
+    
+    if(emailValue === '') {
+        setErrorFor(email, 'No puede dejar el email en blanco');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'No ingreso un email válido');
+    } else {
+        setSuccessFor(email);
+    }
+    
+    if(telefonoValue === '') {
+        setErrorFor(telefonoValue, 'Telefono no debe ingresar en blanco.');
+    } else {
+        setSuccessFor(telefono);
+    }
+    if(consultaValue === '') {
+        setErrorFor(consultaValue, 'Consulta no debe ingresar en blanco.');
+    } else {
+        setSuccessFor(consulta);
+    }
+    
+}
 
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
