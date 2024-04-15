cf4b578fef3e69e3d9a812440222698ab4628d73
abelsanxd <abelsan4665@gmail.com>
2024-04-15 06:51:24 -0300

    se modifico el idioma de la pagina, se cambio la card de detalle de juego

--- js/detalleJuego.js
+++ js/detalleJuego.js
@@ -20,19 +20,24 @@ console.log(juegoBuscado);
 
 const seccionDetalle = document.querySelector("#seccion-detalle");
 seccionDetalle.innerHTML = `
-<div class="card mb-3 border m-5">
+<div class="postcard light blue mb-3 border m-5">
     <div class="row g-0">
-        <div class="col-md-4">
-            <img src="${juegoBuscado.imagen}" class="img-fluid w-100 rounded-start" alt="...">
+        <div class="col-md-4 ps-0">
+            <a class="postcard__img_link" href="#">
+                <img src="${juegoBuscado.imagen}" class="postcard__img img-fluid rounded-start" alt="Podcast Image">
+            </a>
         </div>
-        <div class="col-md-6 fs-3">
-            <div class="card-body">
-                <h5 class="card-title fs-2 textoDetalle">${juegoBuscado.titulo}</h5>
-                <p class="card-text fs-5">${juegoBuscado.descripcion}</p>
-                <p class="card-text fs-5">Categoria: ${juegoBuscado.categoria}</p>
-                <p class="card-text fs-5">Desarrollador: ${juegoBuscado.desarrollador}</p>
-                <p class="card-text fs-5">Requisitos del sistema: ${juegoBuscado.requisitoSistema}</p>
-                <p class="card-text fs-5">Precio: ${juegoBuscado.precio} USD</p>
+        <div class="col-md-8">
+            <div class="postcard__text">
+                <h1 class="postcard__title blue"><a href="#" class="card-title fs-2 textoDetalle">${juegoBuscado.titulo}</a></h1>
+                <div class="postcard__bar"></div>
+                <div class="postcard__preview-txt card-text fs-5">${juegoBuscado.descripcion}</div>
+                <ul class="postcard__tagbox">
+                    <li class="tag__item"><i class="fas fa-tag mr-2"></i>Categoria: ${juegoBuscado.categoria}</li>
+                    <li class="tag__item"><i class="fas fa-user mr-2"></i>Desarrollador: ${juegoBuscado.desarrollador}</li>
+                    <li class="tag__item"><i class="fas fa-desktop mr-2"></i>Requisitos del sistema: ${juegoBuscado.requisitoSistema}</li>
+                    <li class="tag__item"><i class="fas fa-dollar-sign mr-2"></i>Precio: ${juegoBuscado.precio} USD</li>
+                </ul>
             </div>
         </div>
     </div>
